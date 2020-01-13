8e17c93c1ff1543d954d69e4c9e001aa67f2f41c
jessica suelen claudino de andrade <jessica.dev.andrade@gmail.com>
2020-01-12 23:25:34 -0300

    adição de icons e splashs, modificação na estrutura do status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,16 @@ import { IndexPage } from '../pages/index/index';
 export class MyApp {
   rootPage: any = HomePage;
   pages: Array<{title:string, component: any}>;
+  statusBar: any;
+  platform: any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      if (this.platform.is('android')) {
+        this.statusBar.backgroundColorByHexString("#33000000");
+      }
       statusBar.styleDefault();
       splashScreen.hide();
     });
